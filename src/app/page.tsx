80b63396fbdf58ab1c39c017af8d64ae177b9a8b
renskiedulog <22104607@gmail.com>
2024-03-19 10:57:47 +0800

    API Check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
-import Image from "next/image";
+import makeRequest from "@/utils/requests";
+import Link from "next/link";
 
-export default function Home() {
-  return <main>asd</main>;
+export default async function Home() {
+  const mangas = await makeRequest("manga");
+  console.log(mangas);
+  return (
+    <main>
+      {mangas?.data?.map((manga: any) => (
+        <Link href="#">{manga?.id}</Link>
+      ))}
+    </main>
+  );
 }
---
+++ src/utils/requests.ts
@@ -0,0 +1,33 @@
+const baseUrl = "http://localhost:3000";
+
+const makeRequest = async (
+  endpoint: string,
+  params?: object,
+  filter?: object,
+  options?: object
+): Promise<any> => {
+  try {
+    let queryString = "";
+    if (filter) {
+      queryString = Object.entries(filter)
+        .map(([key, value]) => `order[${key}]=${value}`)
+        .join("&");
+    }
+
+    const url = `${baseUrl}/api/${endpoint}${
+      queryString ? `?${queryString}` : ""
+    }`;
+
+    console.log("Request URL:", url); // Add this line for debugging
+
+    const req = await fetch(url, options);
+    const res = await req.json();
+
+    return res;
+  } catch (error) {
+    console.error("Request failed:", error);
+    return [];
+  }
+};
+
+export default makeRequest;
