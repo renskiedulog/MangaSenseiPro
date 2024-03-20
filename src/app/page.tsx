ce59fa9a8c8e017b76d80e039a278a4c4cab8c86
renskiedulog <22104607@gmail.com>
2024-03-20 10:47:49 +0800

    Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import MangaCarousel from "@/components/Components/MangaCarousel";
+import { Carousel } from "@/utils/requests";
 
 export default async function Home() {
+  "use server";
+  const carouselItems = await Carousel();
+
   return (
     <main className="min-h-[90vh]">
-      <MangaCarousel />
+      <MangaCarousel carouselItems={carouselItems} />
     </main>
   );
 }
