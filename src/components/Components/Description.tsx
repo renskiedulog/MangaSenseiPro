148cc5ef2266c3e81dea21f1d688f849ab11cd2b
renskiedulog <renato.dulog@webriq.services>
2024-04-29 17:10:36 +0800

    Fix: Description Component

--- src/components/Components/Description.tsx
+++ src/components/Components/Description.tsx
@@ -1,14 +1,40 @@
 "use client";
-import { useState  } from 'react'
+import { useState, useRef, useEffect } from "react";
 
 const Description = ({ description }: any) => {
   const [toggle, setToggle] = useState(false);
+  const contentRef = useRef<HTMLDivElement>(null);
+  const [isOverflowing, setIsOverflowing] = useState(false);
+
+  useEffect(() => {
+    if (contentRef.current) {
+      setIsOverflowing(
+        contentRef.current.clientHeight < contentRef.current.scrollHeight
+      );
+    }
+  }, [description]);
 
   return (
-    <div className='w-full'>
-      <p className="text-primary text-base md:text-lg font-bold">Description:</p>
-      <div className={`${toggle ? "" : "line-clamp-4"} !text-[15px] opacity-80 md:text-base`}>{description}</div>
-      <button className="opacity-60 py-2 uppercase text-left w-full font-medium hover:text-blue-500" onClick={() => setToggle(!toggle)}>{toggle ? "Show Less" : "Show More"}</button>
+    <div className="w-full">
+      <p className="text-primary text-base md:text-lg font-bold">
+        Description:
+      </p>
+      <div
+        className={`${
+          toggle ? "" : "line-clamp-6"
+        } !text-[15px] opacity-80 md:text-base`}
+        ref={contentRef}
+      >
+        {description}
+      </div>
+      {isOverflowing && (
+        <button
+          className="opacity-60 py-2 uppercase text-left w-full font-medium hover:text-blue-500"
+          onClick={() => setToggle(!toggle)}
+        >
+          {toggle ? "Show Less" : "Show More"}
+        </button>
+      )}
     </div>
   );
 };
